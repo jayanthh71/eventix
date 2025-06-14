7dba0129a1935afa2c231169523c8316f85020e1
jayanthh71 <jayanth37069@gmail.com>
2025-06-15 01:21:36 +0400

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="font-anek flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center gap-4 font-bold text-white">
+      <h1 className="text-9xl">404</h1>
+      <p className="text-lg">Page Not Found</p>
+    </div>
+  );
+}
